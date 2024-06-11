02ef95952902ca9e22f5173fed342810182d0984
Douglas R. Lee <douglasrlee@users.noreply.github.com>
2024-06-10 21:24:22 -0500

    Styling the rest of the devise html view and adding a navigation bar
    Closes #1

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,22 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap"
+
+//#region auto favicon
+const usesDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches || false;
+const favicon = document.getElementById('favicon');
+
+function switchIcon(usesDarkMode) {
+  if (usesDarkMode) {
+    favicon.href = '/dark/dark-logo.png';
+  } else {
+    favicon.href = '/light/light-logo.png';
+  }
+}
+
+window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (e) => switchIcon(e.matches));
+
+switchIcon(usesDarkMode);
+//#endregion
