3fdcf794915f484d706c5c4b1f0d17048d567981
Douglas R. Lee <douglas.r.lee@icloud.com>
2024-11-04 16:47:29 -0600

    Initial spring boot application

---
+++ src/main/java/com/douglasrlee/paychecq/PayChecQApplication.java
@@ -0,0 +1,11 @@
+package com.douglasrlee.paychecq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayChecQApplication {
+  public static void main(String[] arguments) {
+    SpringApplication.run(PayChecQApplication.class, arguments);
+  }
+}
---
+++ src/test/java/com/douglasrlee/paychecq/PayChecQApplicationTests.java
@@ -0,0 +1,11 @@
+package com.douglasrlee.paychecq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayChecQApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
