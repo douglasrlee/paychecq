9d177016ca60241ad4504d05a8529c2dd622d7a0
Douglas R. Lee <douglas.r.lee@icloud.com>
2024-11-19 19:34:03 -0600

    Styling login page and adding simple form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :timeoutable
+
+  validates :name, presence: true
 end
