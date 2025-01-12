7aeaf67356404f8dce873f93ef9ed2db8534d6fc
Douglas R. Lee <douglasrlee@users.noreply.github.com>
2025-01-12 00:46:22 -0600

    Adding strong migrations
    Closes #20 
    Fixes #4

---
+++ config/initializers/strong_migrations.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+StrongMigrations.start_after = 20_250_112_064_306
+StrongMigrations.lock_timeout = 10.seconds
+StrongMigrations.statement_timeout = 1.hour
+StrongMigrations.auto_analyze = true
+StrongMigrations.target_version = 16
