f9b10e1385668c29b21c79bff30e64c8664ef364
Douglas R. Lee <douglas.r.lee@icloud.com>
2025-02-23 23:30:11 -0600

    Consolidating solid databases

---
+++ db/migrate/20250224051045_create_solid_queue_processes.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class CreateSolidQueueProcesses < ActiveRecord::Migration[8.0]
+  def change
+    create_table :solid_queue_processes do |t|
+      t.string :kind, null: false
+      t.datetime :last_heartbeat_at, null: false
+      t.bigint :supervisor_id
+      t.integer :pid, null: false
+      t.string :hostname
+      t.text :metadata
+      t.datetime :created_at, null: false
+      t.string :name, null: false
+
+      t.index :last_heartbeat_at, name: 'index_solid_queue_processes_on_last_heartbeat_at'
+      t.index [ :name, :supervisor_id ], name: 'index_solid_queue_processes_on_name_and_supervisor_id', unique: true
+      t.index :supervisor_id, name: 'index_solid_queue_processes_on_supervisor_id'
+    end
+  end
+end
