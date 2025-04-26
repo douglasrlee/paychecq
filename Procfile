728d71fac2fe13d642b01d597ec0ba5bd7e84e7e
Douglas R. Lee <douglasrlee@users.noreply.github.com>
2025-04-26 00:39:53 -0500

    Strong migrations, bullet, rubocop, paper trail, solid database consalidation & heroku setup
    Closes #3

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: bin/rails server
+worker: bin/rails solid_queue:start
+release: bundle exec rails db:migrate
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  has_paper_trail
 end
