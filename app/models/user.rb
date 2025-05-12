2fc8a092ca7823302a19931056a2dc0638bcdbea
Douglas Lee <douglas.r.lee@icloud.com>
2025-05-12 17:32:37 -0500

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,5 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :confirmable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  def send_devise_notification(notification, *)
-    devise_mailer.send(notification, self, *).deliver_later(retry_on: RuntimeError, wait: 5.seconds, attempts: :unlimited)
-  end
+  validates :name, presence: true
 end
