0ae3b546ce6e50ff09a6687dfdd3f96c21dc4f5a
Douglas R. Lee <douglas.r.lee@icloud.com>
2025-05-13 20:53:00 -0500

    Adding tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid user' do
+    user = User.new(name: 'John Doe', email: 'johndoe@example.com', password: 'P@ssw0rd!')
+
+    assert(user.valid?)
+  end
+
+  test 'invalid user without name' do
+    user = User.new(name: nil, email: 'johndoe@example.com', password: 'P@ssw0rd!')
+
+    assert(user.invalid?)
+    assert_equal(user.errors[:name], [ 'can\'t be blank' ])
+  end
 end
--- test/temp_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class TempTest < Minitest::Test
-  def temp_test
-    assert_equal(3, 1 + 2)
-  end
-end
