a3fd01e6d6d0c138411ad3740961c2e15c605da9
Douglas R. Lee <douglasrlee@users.noreply.github.com>
2025-05-13 20:56:27 -0500

    Styling devise pages
    Closes #19

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_paper_trail_whodunnit
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   allow_browser versions: :modern
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+  end
 end
