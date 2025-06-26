5fea4cf110127c39178d1e9b731fd619dc18c000
Douglas R. Lee <douglasrlee@users.noreply.github.com>
2025-06-26 00:05:50 -0500

    Adding bullet, strong migrations, & setting up uuids
    Closes #12

---
+++ config/initializers/strong_migrations.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+StrongMigrations.start_after = 20_250_625_210_234
+StrongMigrations.lock_timeout = 10.seconds
+StrongMigrations.statement_timeout = 1.hour
+StrongMigrations.auto_analyze = true
+StrongMigrations.target_version = 16
