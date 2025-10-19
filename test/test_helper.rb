1fec8a8eaf001c968a29f71ad4de1f5f30318c34
Douglas R. Lee <douglasrlee@users.noreply.github.com>
2025-10-18 23:14:58 -0500

    Adding bullet gem
    Closes #7

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,18 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+
+    def before_setup
+      Bullet.start_request
+
+      super
+    end
+
+    def after_teardown
+      super
+
+      Bullet.perform_out_of_channel_notifications if Bullet.notification?
+      Bullet.end_request
+    end
   end
 end
