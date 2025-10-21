05c287999ab2beb007b612319e868f7d90255ef3
Douglas R. Lee <douglas.r.lee@icloud.com>
2025-10-21 00:59:05 -0500

    Adding test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
+
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
@@ -17,6 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     terminate_session
+
     redirect_to new_session_path
   end
 end
