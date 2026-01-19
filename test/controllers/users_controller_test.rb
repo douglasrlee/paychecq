6a94d6f8a763baccb6b34364a1dad2a5e7141b16
Douglas R. Lee <douglas.r.lee@icloud.com>
2026-01-19 00:32:55 -0600

    Adding tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,58 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    post users_create_url
+  test "new" do
+    get new_user_path
+
     assert_response :success
   end
 
-  test "should get new" do
-    get users_new_url
-    assert_response :success
+  test "create with valid params" do
+    assert_difference([ "User.count", "Session.count" ]) do
+      post users_path, params: {
+        user: {
+          first_name: "Test",
+          last_name: "User",
+          email_address: "newuser@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+
+    assert_redirected_to root_path
+    assert cookies[:session_id]
+  end
+
+  test "create with invalid params" do
+    assert_no_difference("User.count") do
+      post users_path, params: {
+        user: {
+          first_name: "",
+          last_name: "",
+          email_address: "",
+          password: "",
+          password_confirmation: ""
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  test "create with mismatched passwords" do
+    assert_no_difference("User.count") do
+      post users_path, params: {
+        user: {
+          first_name: "Test",
+          last_name: "User",
+          email_address: "newuser@example.com",
+          password: "password",
+          password_confirmation: "different"
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
   end
 end
