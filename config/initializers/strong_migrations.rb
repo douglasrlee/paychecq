3ebbc545effebf13b15f7d59632c667c210c236d
Douglas R. Lee <douglas.r.lee@icloud.com>
2026-01-20 01:22:07 -0600

    rubocop

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,5 +1,5 @@
 # Mark existing migrations as safe
-StrongMigrations.start_after = 20_260_116_165_016
+StrongMigrations.start_after = 20260116165016
 
 # Set timeouts for migrations
 # If you use PgBouncer in transaction mode, delete these lines and set timeouts on the database user
