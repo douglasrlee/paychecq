b762f1dbe0f4e99b89b58c9c6122d2b013834c86
Douglas R. Lee <douglasrlee@users.noreply.github.com>
2026-01-23 00:26:40 -0600

    Increase mail job retry back off
    Closes #49

--- app/jobs/mail_delivery_job.rb
+++ app/jobs/mail_delivery_job.rb
@@ -1,3 +1,7 @@
 class MailDeliveryJob < ActionMailer::MailDeliveryJob
-  retry_on StandardError, wait: :polynomially_longer, attempts: 5
+  # Retry with longer intervals for SMTP outages
+  # Attempts: 1min, 5min, 15min, 30min, 1hr, 2hr, 4hr (total ~8 hours)
+  retry_on StandardError, attempts: 8, wait: lambda { |executions|
+    [ 1, 5, 15, 30, 60, 120, 240 ].fetch(executions - 2, 240).minutes
+  }
 end
