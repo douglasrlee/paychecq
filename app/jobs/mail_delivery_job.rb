e70775dcd08dd58b7746a9424681f8c22fa19c17
Douglas R. Lee <douglas.r.lee@icloud.com>
2026-01-23 00:20:31 -0600

    PR Comments

--- app/jobs/mail_delivery_job.rb
+++ app/jobs/mail_delivery_job.rb
@@ -2,6 +2,6 @@ class MailDeliveryJob < ActionMailer::MailDeliveryJob
   # Retry with longer intervals for SMTP outages
   # Attempts: 1min, 5min, 15min, 30min, 1hr, 2hr, 4hr (total ~8 hours)
   retry_on StandardError, attempts: 8, wait: lambda { |executions|
-    [ 1, 5, 15, 30, 60, 120, 240 ].fetch(executions - 1, 240).minutes
+    [ 1, 5, 15, 30, 60, 120, 240 ].fetch(executions - 2, 240).minutes
   }
 end
