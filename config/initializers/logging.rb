c0a17489823c577cf1f19ccad2e5520e74523f7c
Douglas R. Lee <douglas.r.lee@icloud.com>
2026-01-22 23:12:58 -0600

    Trying this

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,13 +1,13 @@
 Rails.application.config.after_initialize do
   if Rails.env.local?
     stdout_logger = ActiveSupport::Logger.new($stdout)
+
     Rails.logger.broadcast_to(stdout_logger)
+
+    SolidQueue.logger = Rails.logger
   else
     appsignal_logger = Appsignal::Logger.new('rails')
-    appsignal_logger.level = Rails.logger.level
-    Rails.logger.broadcast_to(appsignal_logger)
-  end
 
-  SolidQueue.logger = Rails.logger
-  ActiveJob::Base.logger = Rails.logger
+    Rails.logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
+  end
 end
