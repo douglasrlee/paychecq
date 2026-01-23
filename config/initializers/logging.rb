9fcade7581c033b3398a7ba2f677f8b39a266e0b
Douglas R. Lee <douglasrlee@users.noreply.github.com>
2026-01-22 18:13:40 -0600

    Replace scout with app signal
    Closes #44

---
+++ config/initializers/logging.rb
@@ -0,0 +1,12 @@
+if Rails.env.local?
+  Rails.application.config.after_initialize do
+    stdout_logger = ActiveSupport::Logger.new($stdout)
+    Rails.logger.broadcast_to(stdout_logger)
+    SolidQueue.logger = Rails.logger
+  end
+else
+  appsignal_logger = Appsignal::Logger.new('rails')
+  appsignal_logger.level = Logger::Severity::DEBUG
+
+  Rails.logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
+end
