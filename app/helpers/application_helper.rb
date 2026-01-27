100e047bfab3d54cf74b393be9ffbd61f8e077e9
Douglas R. Lee <douglasrlee@users.noreply.github.com>
2026-01-26 20:04:27 -0600

    Adding navigation
    Closes #56

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def authentication_page?
+    controller_name == 'sessions' ||
+      (controller_name == 'users' && action_name == 'new') ||
+      (controller_name == 'passwords' && action_name.in?(%w[new edit]))
+  end
+
+  def gravatar_url(email_address, size: 80)
+    hash = Digest::MD5.hexdigest(email_address.to_s.downcase.strip)
+
+    "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=mp"
+  end
 end
---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,104 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  attr_accessor :controller_name, :action_name
+
+  test 'authentication_page? returns true for sessions controller' do
+    @controller_name = 'sessions'
+    @action_name = 'new'
+
+    assert authentication_page?
+
+    @action_name = 'create'
+
+    assert authentication_page?
+
+    @action_name = 'destroy'
+
+    assert authentication_page?
+  end
+
+  test 'authentication_page? returns true for users#new' do
+    @controller_name = 'users'
+    @action_name = 'new'
+
+    assert authentication_page?
+  end
+
+  test 'authentication_page? returns false for users#edit' do
+    @controller_name = 'users'
+    @action_name = 'edit'
+
+    assert_not authentication_page?
+  end
+
+  test 'authentication_page? returns true for passwords#new' do
+    @controller_name = 'passwords'
+    @action_name = 'new'
+
+    assert authentication_page?
+  end
+
+  test 'authentication_page? returns true for passwords#edit' do
+    @controller_name = 'passwords'
+    @action_name = 'edit'
+
+    assert authentication_page?
+  end
+
+  test 'authentication_page? returns false for passwords#update' do
+    @controller_name = 'passwords'
+    @action_name = 'update'
+
+    assert_not authentication_page?
+  end
+
+  test 'authentication_page? returns false for other controllers' do
+    @controller_name = 'transactions'
+    @action_name = 'index'
+
+    assert_not authentication_page?
+
+    @controller_name = 'home'
+    @action_name = 'index'
+
+    assert_not authentication_page?
+  end
+
+  test 'gravatar_url generates correct URL with default size' do
+    email = 'test@example.com'
+    expected_hash = Digest::MD5.hexdigest(email)
+    url = gravatar_url(email)
+
+    assert_equal "https://www.gravatar.com/avatar/#{expected_hash}?s=80&d=mp", url
+  end
+
+  test 'gravatar_url generates correct URL with custom size' do
+    email = 'test@example.com'
+    expected_hash = Digest::MD5.hexdigest(email)
+    url = gravatar_url(email, size: 200)
+
+    assert_equal "https://www.gravatar.com/avatar/#{expected_hash}?s=200&d=mp", url
+  end
+
+  test 'gravatar_url downcases email before hashing' do
+    lowercase_hash = Digest::MD5.hexdigest('test@example.com')
+    url = gravatar_url('TEST@EXAMPLE.COM')
+
+    assert_includes url, lowercase_hash
+  end
+
+  test 'gravatar_url strips whitespace from email' do
+    stripped_hash = Digest::MD5.hexdigest('test@example.com')
+    url = gravatar_url('  test@example.com  ')
+
+    assert_includes url, stripped_hash
+  end
+
+  test 'gravatar_url handles nil email' do
+    expected_hash = Digest::MD5.hexdigest('')
+    url = gravatar_url(nil)
+
+    assert_equal "https://www.gravatar.com/avatar/#{expected_hash}?s=80&d=mp", url
+  end
+end
