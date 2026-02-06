7c40ecb9bf5b95c0bfb680ab41a878a746428ca0
Douglas R. Lee <douglas.r.lee@icloud.com>
2026-02-05 21:22:45 -0600

    Adding mission control

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,18 @@
+class AdminController < ApplicationController
+  before_action :require_admin
+
+  private
+
+  def require_admin
+    redirect_to main_app.root_path, alert: 'Not authorized' unless Current.user&.admin?
+  end
+
+  def request_authentication
+    session[:return_to_after_authenticating] = request.url
+    redirect_to main_app.new_session_path
+  end
+
+  def after_authentication_url
+    session.delete(:return_to_after_authenticating) || main_app.root_url
+  end
+end
