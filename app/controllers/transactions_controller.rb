a75754012771509a650b76721733ee5e55c7247a
Douglas R. Lee <douglas.r.lee@icloud.com>
2026-02-08 23:05:11 -0600

    PR Comments

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
   def index
-    @transactions = Current.user.transactions.order(Arel.sql("date DESC NULLS LAST, created_at DESC"))
-    @has_bank = Current.user.banks.exists?
+    @transactions = Current.user.transactions.order(Arel.sql('date DESC NULLS LAST, created_at DESC')).load
+    @has_bank = Current.user.banks.exists? if @transactions.empty?
   end
 end
--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -1,13 +1,36 @@
 require 'test_helper'
 
 class TransactionsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    sign_in_as(users(:johndoe))
+  test 'index requires authentication' do
+    get transactions_url
+
+    assert_redirected_to new_session_path
   end
 
-  test 'should get index' do
+  test 'index shows transactions' do
+    sign_in_as(users(:johndoe))
+
     get transactions_url
 
     assert_response :success
+  end
+
+  test 'index shows empty state when no bank linked' do
+    sign_in_as(users(:admin))
+
+    get transactions_url
+
+    assert_response :success
+    assert_select 'p', text: 'No bank account linked'
+    assert_select 'a', text: 'Link Account'
+  end
+
+  test 'index shows empty state when bank linked but no transactions' do
+    sign_in_as(users(:johndoe))
+
+    get transactions_url
+
+    assert_response :success
+    assert_select 'p', text: 'No transactions yet'
   end
 end
