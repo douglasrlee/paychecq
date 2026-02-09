45e4782f91850551dc66fe04b23efbf874ea6dfa
Douglas R. Lee <douglas.r.lee@icloud.com>
2026-02-08 23:01:17 -0600

    wip

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
   def index
-    @transactions = Current.user.transactions.order(date: :desc, created_at: :desc)
+    @transactions = Current.user.transactions.order(Arel.sql("date DESC NULLS LAST, created_at DESC"))
     @has_bank = Current.user.banks.exists?
   end
 end
